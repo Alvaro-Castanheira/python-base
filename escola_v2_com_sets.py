d99f775f5fea41fe51c52d2b701001bb53165643
Álvaro Castanheira <alvaro.miguel.100@gmail.com>
2026-04-14 11:44:34 +0100

    adicionados exercicios com listas , sets e dic

---
+++ escola_v2_com_sets.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Relatório de crianças por atividade
+
+Imprimir a lista de crianças agrupadas por sala
+que frequentam cada uma das atividades.
+"""
+__version__ = "0.0.2"
+
+# Dados
+sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
+sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Julia"]
+
+aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
+aula_musica = ["Erik", "Carlos", "Maria"]
+aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+
+atividades = [
+    ("Inglês",aula_ingles),
+    ("Música",aula_musica),
+    ("Dança", aula_danca),
+]
+
+# Listar alunos em cada atividade por sala
+
+for nome_atividade, atividade in atividades:
+
+    print(f"Alunos da atividade {nome_atividade}")
+
+    #sala que tem interseção com a atividade
+    
+    atividade_sala1 = set(sala1) & set(atividade)
+    atividade_sala2 = set(sala2).intersection(set(atividade))
+
+    print("sala 1:" ,atividade_sala1)
+    print("sala 2:",atividade_sala2)        
+    print("-"*40)
